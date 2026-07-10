9e314865d2b473faa8f3a64b8ef77834f501fa1d
smowebdev <smowebdev@gmail.com>
2026-07-10 17:19:32 +0700

    create done 5 sections for de extinction page

--- js/de-extinction.js
+++ js/de-extinction.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function ($) {
+  // Mission Slider - Start
   const missionSlider = new Swiper(".de-ex-mission-slider", {
     slidesPerView: "auto",
     spaceBetween: 16,
@@ -14,6 +15,9 @@ jQuery(document).ready(function ($) {
 
     speed: 600,
   });
+  // Mission Slider - End
+
+  // Rebuilding Species Active - Start
   $(".rebuilding-species-item").on("click", function () {
     if ($(this).hasClass("active")) return;
 
@@ -21,28 +25,28 @@ jQuery(document).ready(function ($) {
 
     $(this).addClass("active");
   });
-});
-$(function () {
-  const $wrap = $(".de-ex-hiw-inner");
+  // Rebuilding Species Active - End
 
-  const dividers = [
-    $(".divider-1"),
-    $(".divider-2"),
-    $(".divider-3"),
-    $(".divider-4"),
-    $(".divider-5"),
+  // How It Work - Before After Image - Start
+  const $hiwWrap = $(".de-ex-hiw-inner");
+
+  const hiwDividers = [
+    $(".de-ex-hiw-inner .divider-1"),
+    $(".de-ex-hiw-inner .divider-2"),
+    $(".de-ex-hiw-inner .divider-3"),
+    $(".de-ex-hiw-inner .divider-4"),
+    $(".de-ex-hiw-inner .divider-5"),
   ];
 
-  const imgs = [
-    $(".img-1"),
-    $(".img-2"),
-    $(".img-3"),
-    $(".img-4"),
-    $(".img-5"),
+  const hiwImages = [
+    $(".de-ex-hiw-inner .img-1"),
+    $(".de-ex-hiw-inner .img-2"),
+    $(".de-ex-hiw-inner .img-3"),
+    $(".de-ex-hiw-inner .img-4"),
+    $(".de-ex-hiw-inner .img-5"),
   ];
 
-  // vị trí mặc định (%)
-  let pos = [20, 40, 60, 80, 100];
+  let pos = [6, 26, 46, 66, 86];
 
   let dragging = -1;
 
@@ -62,9 +66,9 @@ $(function () {
     if (e.type === "touchmove") pageX = e.originalEvent.touches[0].pageX;
     else pageX = e.pageX;
 
-    let offset = $wrap.offset().left;
+    let offset = $hiwWrap.offset().left;
 
-    let w = $wrap.width();
+    let w = $hiwWrap.width();
 
     let percent = ((pageX - offset) / w) * 100;
 
@@ -82,43 +86,124 @@ $(function () {
     dragging = -1;
   });
   function update() {
-    dividers.forEach(function ($el, i) {
+    hiwDividers.forEach(function ($el, i) {
       $el.css("left", pos[i] + "%");
     });
-    $(".level")
-      .eq(0)
-      .css("left", pos[0] + "%");
-    $(".level")
-      .eq(1)
-      .css("left", pos[1] + "%");
-    $(".level")
-      .eq(2)
-      .css("left", pos[2] + "%");
-    $(".level")
-      .eq(3)
-      .css("left", pos[3] + "%");
-    $(".level")
-      .eq(4)
-      .css("left", pos[4] + "%"); // clip image 1
-    imgs[0].css(
+
+    hiwImages[0].css(
       "clip-path",
       `polygon(0 0, ${pos[0]}% 0, ${pos[0]}% 100%, 0 100%)`,
-    ); // image2
-    imgs[1].css(
+    );
+    hiwImages[1].css(
       "clip-path",
       `polygon(${pos[0]}% 0, ${pos[1]}% 0, ${pos[1]}% 100%, ${pos[0]}% 100%)`,
-    ); // image3
-    imgs[2].css(
+    );
+    hiwImages[2].css(
       "clip-path",
       `polygon(${pos[1]}% 0, ${pos[2]}% 0, ${pos[2]}% 100%, ${pos[1]}% 100%)`,
-    ); // image4
-    imgs[3].css(
+    );
+    hiwImages[3].css(
       "clip-path",
       `polygon(${pos[2]}% 0, ${pos[3]}% 0, ${pos[3]}% 100%, ${pos[2]}% 100%)`,
-    ); // image5
-    imgs[4].css(
+    );
+    hiwImages[4].css(
       "clip-path",
       `polygon(${pos[3]}% 0, 100% 0, 100% 100%, ${pos[3]}% 100%)`,
     );
   }
+  // How It Work - Before After Image - End
+
+  // De Extinction Process - Start
+  const $deProcessContainer = $(".process-container");
+  const $deSteps = $(".process-step");
+  const $deProcessItems = $(".process-item");
+
+  function updateStep() {
+    const scrollTop = $deProcessContainer.scrollTop();
+    const checkPoint = scrollTop + $deProcessContainer.innerHeight() * 0.4;
+
+    $deProcessItems.each(function () {
+      const top = this.offsetTop;
+      const bottom = top + this.offsetHeight;
+
+      if (checkPoint >= top && checkPoint < bottom) {
+        const step = $(this).data("step");
+
+        $deSteps.removeClass("active");
+        $('.process-step[data-step="' + step + '"]').addClass("active");
+
+        return false;
+      }
+    });
+  }
+
+  updateStep();
+
+  $deProcessContainer.on("scroll", updateStep);
+
+  $deSteps.on("click", function () {
+    const step = $(this).data("step");
+    const target = $('.process-item[data-step="' + step + '"]')[0];
+
+    $deProcessContainer.stop().animate(
+      {
+        scrollTop: target.offsetTop - 40,
+      },
+      600,
+    );
+  });
+
+  $(".process-item").each(function () {
+    const $process = $(this);
+
+    $process.find(".process-faq-answer").hide();
+
+    $process.find(".process-faq-item").first().addClass("active");
+    $process
+      .find(".process-faq-item")
+      .first()
+      .find(".process-faq-answer")
+      .show();
+  });
+
+  $(document).on("click", ".process-faq-question", function () {
+    const $item = $(this).closest(".process-faq-item");
+    const $process = $item.closest(".process-item");
+    const $answer = $item.find(".process-faq-answer");
+
+    if ($answer.is(":visible")) {
+      $item.removeClass("active");
+      $answer.stop(true, true).slideUp(300);
+    } else {
+      $process.find(".process-faq-item.active").removeClass("active");
+
+      $process
+        .find(".process-faq-answer:visible")
+        .stop(true, true)
+        .slideUp(300);
+
+      $item.addClass("active");
+      $answer.stop(true, true).slideDown(300);
+    }
+  });
+  // De Extinction Process - End
+
+  // Ethics Risks Limitations  Slider - Start
+  const $wrapperSlider = $(".ethics-risks-limitations-slider .swiper-wrapper");
+  const $slides = $wrapperSlider.children(".swiper-slide");
+
+  for (let i = 0; i < 1; i++) {
+    $slides.clone().appendTo($wrapperSlider);
+  }
+
+  const ethicsRisksLimitationsSlider = new Swiper(
+    ".ethics-risks-limitations-slider",
+    {
+      slidesPerView: 1.73,
+      centeredSlides: true,
+      loop: true,
+      slideToClickedSlide: true,
+    },
+  );
+  // Ethics Risks Limitations  Slider - End
 });
